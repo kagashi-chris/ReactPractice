d732c4f3c3a71cdb5045f85f087e8c2982ce4392
Chris Zhen <chris.zhenartist@gmail.com>
2021-10-13 02:52:13 -0400

    modal show up when cart button is pressed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [carIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {carIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
